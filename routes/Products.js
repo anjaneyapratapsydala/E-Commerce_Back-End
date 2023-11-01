445e846217d5e1609f8ee342734f8b865801a79e
SYDALA ANJANEYA PRATAP <anjaanjaneyapratapsydala@gmail.com>
2023-11-02 02:12:37 +0530

    Done Basic API's

---
+++ routes/Products.js
@@ -0,0 +1,13 @@
+const {createProduct, fetchAllProducts, fetchProductById, updateProduct} = require('../controller/Product')
+
+const express = require('express')
+
+const router = express.Router();
+
+//products is already added in base path
+router.post('/',createProduct)
+      .get('/',fetchAllProducts) 
+      .get('/:id',fetchProductById)
+      .patch('/:id',updateProduct)
+exports.router = router;
+
